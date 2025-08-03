dac6132a70db123a87e4ef3392e0df0d00ed3b2c
vkyas <124316178+vkyas@users.noreply.github.com>
2025-08-04 00:32:43 +0700

    Add driver for direct kernel memory read/write

--- drivers/kamid/compat.c
+++ drivers/kamid/compat.c
@@ -4,18 +4,13 @@
 #include "memory.h"
 #include "process.h"
 
-extern bool is_driver_verified(void);
-extern long handle_module_base(unsigned long arg);
-
+extern long dispatch_ioctl(struct file *const file, unsigned int const cmd, unsigned long const arg);
 long dispatch_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
-    if (cmd != OP_INIT_KEY && !is_driver_verified()) {
-        return -EPERM;
+    if (cmd == OP_INIT_KEY) {
+        return dispatch_ioctl(filp, cmd, arg);
     }
 
     switch (cmd) {
-        case OP_INIT_KEY: {
-            return filp->f_op->unlocked_ioctl(filp, cmd, arg);
-        }
         case OP_READ_MEM: {
             COPY_MEMORY32 __user *p32 = compat_ptr(arg);
             COPY_MEMORY cm64;
@@ -29,8 +24,7 @@ long dispatch_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long ar
             if (get_user(buffer_ptr32, &p32->buffer)) return -EFAULT;
             cm64.buffer = compat_ptr(buffer_ptr32);
             
-            if (!read_process_memory(cm64.pid, cm64.addr, cm64.buffer, cm64.size)) return -EFAULT;
-            break;
+            return dispatch_ioctl(filp, OP_READ_MEM, (unsigned long)&cm64);
         }
         case OP_WRITE_MEM: {
             COPY_MEMORY32 __user *p32 = compat_ptr(arg);
@@ -45,22 +39,23 @@ long dispatch_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long ar
             if (get_user(buffer_ptr32, &p32->buffer)) return -EFAULT;
             cm64.buffer = compat_ptr(buffer_ptr32);
 
-            if (!write_process_memory(cm64.pid, cm64.addr, cm64.buffer, cm64.size)) return -EFAULT;
-            break;
+            return dispatch_ioctl(filp, OP_WRITE_MEM, (unsigned long)&cm64);
         }
         case OP_MODULE_BASE: {
             MODULE_BASE32 __user *p32 = compat_ptr(arg);
             MODULE_BASE mb64;
             compat_uptr_t name_ptr32;
             char name_buffer[256];
+            char __user *user_name_ptr;
 
             if (get_user(mb64.pid, &p32->pid)) return -EFAULT;
             if (get_user(name_ptr32, &p32->name)) return -EFAULT;
-            mb64.name = compat_ptr(name_ptr32);
 
-            if (copy_from_user(name_buffer, mb64.name, sizeof(name_buffer) - 1) != 0) return -EFAULT;
+            user_name_ptr = compat_ptr(name_ptr32);
+
+            if (copy_from_user(name_buffer, user_name_ptr, sizeof(name_buffer) - 1) != 0) return -EFAULT;
             name_buffer[sizeof(name_buffer) - 1] = '\0';
-
+            
             mb64.base = get_module_base(mb64.pid, name_buffer);
 
             if (put_user(mb64.base, &p32->base)) return -EFAULT;
